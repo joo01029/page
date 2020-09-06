9c95578e41295b179c4bc3c817a9becef7bf5a79
joo01029 <joo01029@naver.com>
2020-09-06 15:29:04 +0900

    ss

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-function scroll_a(){
-    var location = document.querySelector("#first").offsetTop;
+function scroll1(){
+    var location = document.querySelector("#one").offsetTop;
     location -= 300;
     window.scrollTo({top:location, behavior:'smooth'});
 }
-function scroll_b(id){
-    var location = document.querySelector("#second").offsetTop;
+function scroll2(){
+    var location = document.querySelector("#two").offsetTop;
+    location -= 100;
+    window.scrollTo({top:location, behavior:'smooth'});
+}
+function scroll3(){
+    var location = document.querySelector("#three").offsetTop;
     location -= 300;
     window.scrollTo({top:location, behavior:'smooth'});
 }
-function scroll_c(id){
-    var location = document.querySelector("#third").offsetTop;
+function scroll4(){
+    var location = document.querySelector("#four").offsetTop;
     location -= 300;
+    window.scrollTo({top:location, behavior:'smooth'});
+}
+function scroll5(){
+    var location = document.querySelector("#five").offsetTop;
+    location -= 300;
+    window.scrollTo({top:location, behavior:'smooth'});
+}
+function scroll6(){
+    
+    var location = document.querySelector("#six").offsetTop;
+    location -= 100;
+    window.scrollTo({top:location, behavior:'smooth'});
+}
+function scroll7(){
+    
+    var location = document.querySelector("#seven").offsetTop;
+    location -= 200;
+    window.scrollTo({top:location, behavior:'smooth'});
+}
+function scroll8(){
+    
+    var location = document.querySelector("#eight").offsetTop;
+    location -= 200;
+    window.scrollTo({top:location, behavior:'smooth'});
+}
+function scroll9(){
+    
+    var location = document.querySelector("#nine").offsetTop;
+    location -= 100;
     window.scrollTo({top:location, behavior:'smooth'});
 }
